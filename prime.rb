39342475ee5b2dbc88c8f64b0685c010082ec6d9
github username <github email address>
2018-06-01 03:19:14 -0400

    Implementing Prime

--- prime.rb
+++ prime.rb
@@ -1,1 +1,25 @@
-# Add  code here!
+def prime?(integer)
+
+    if integer <= 1 
+        return false
+    elsif integer <= 3
+        return true
+    elsif integer % 2 == 0 || integer % 3 == 0
+        return false
+    end
+
+    denominator = 5
+
+    while denominator ** 2 < integer
+        if integer % denominator == 0 || integer % (denominator + 2) == 0
+            return false
+        end
+
+        denominator += 6
+    end
+
+    return true
+
+     
+
+end
